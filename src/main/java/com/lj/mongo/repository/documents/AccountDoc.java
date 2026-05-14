0bc56f2eea8e0af4717f6b46a3a043a9360da32d
Lukasz-Jan <lukasz_jan@vp.pl>
2026-05-14 17:06:42 +0200

    change to postgres

--- src/main/java/com/lj/mongo/repository/documents/AccountDoc.java
+++ src/main/java/com/lj/mongo/repository/documents/AccountDoc.java
@@ -1,7 +1,6 @@
 package com.lj.mongo.repository.documents;
 
 import com.lj.gen.json.mappings.transfer.CurrencyAmount;
-import java.util.ArrayList;
 import java.util.List;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
